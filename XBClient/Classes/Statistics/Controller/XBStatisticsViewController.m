42cb47e0b6decb05b2070c032f7d6aa706de84a0
wuxiaofang <wuxiaofang@yongche.com>
2017-10-20 14:32:02 +0800

    refs #23468, My页面

--- XBClient/Classes/Statistics/Controller/XBStatisticsViewController.m
+++ XBClient/Classes/Statistics/Controller/XBStatisticsViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self setCustomLabelForNavTitle:@"教学指标"];
 }
 
 - (void)didReceiveMemoryWarning {
